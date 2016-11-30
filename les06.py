3812acab8390014044d1bf47d6fb0be269d9f64f
shulgaalexey <shulgaalexey@gmail.com>
2016-11-30 23:05:09 +0900

    Added random circle drawing and random movement

--- les06.py
+++ les06.py
@@ -6,11 +6,16 @@ import random
 root = Tkinter.Tk()
 root.withdraw()
 
+turtle.speed(0)
 
 answer = ''
 while answer != 'n':
     answer = tkSimpleDialog.askstring('Draw the circle?', 'y/n')
     if answer == 'y':
-        turtle.circle(random.randrange(1, 100))
+        turtle.penup()
+        turtle.goto(random.randrange(-300, 300), random.randrange(-200, 200))
+        turtle.pendown()
+        for i in range(1, 3):
+            turtle.circle(random.randrange(1, 100))
     else:
         break
