8e2d9f90d67c1977909291ce094e19081d91f57e
shulgaalexey <shulgaalexey@gmail.com>
2016-12-01 21:58:08 +0900

    Russian Roulette game implemented

--- les06.py
+++ les06.py
@@ -21,33 +21,55 @@ def fill_circle(posx, posy, radius, r, g, b):
     draw_circle(posx, posy, radius)
     turtle.end_fill()
 
-fill_circle(0, 0, 80, .8, .8, .8)
-fill_circle(0, 160, 6, .2, .2, .2)
+def draw_baraban(): # draw the bullet baraban
+    fill_circle(0, 0, 80, .8, .8, .8)
+    fill_circle(0, 160, 6, .2, .2, .2)
+
+    phi = 360. / 7
+    r = 50.
 
 
+    # draw the empty bullet holes in baraban
+    for i in range(0, 8):
+        phi_rad = phi * i * math.pi / 180.0
+        fill_circle(math.sin(phi_rad) * r, math.cos(phi_rad) * r + 60, 20, .5, .5, .5)
 
-phi = 360 / 7
-r = 50
 
-for i in range(0, 7):
-    phi_rad = phi * i * math.pi / 180.0
-    #turtle.goto(math.sin(phi_rad) * r, math.cos(phi_rad) * r + 60)
-    #turtle.circle(20)
-    fill_circle(math.sin(phi_rad) * r, math.cos(phi_rad) * r + 60, 20, .5, .5, .5)
+def animate_baraban(start_pos): # animate the rotating baraban
+    phi = 360. / 7
+    r = 50.
 
-fill_circle(math.sin(phi_rad) * r, math.cos(phi_rad) * r + 60, 20, .9, .0, .0)
+    for i in range(0, random.randrange(7, 100)):
+        phi_rad = phi * (i + start_pos) * math.pi / 180.0
+        fill_circle(math.sin(phi_rad) * r, math.cos(phi_rad) * r + 60, 20, .9, .0, .0)
+        fill_circle(math.sin(phi_rad) * r, math.cos(phi_rad) * r + 60, 20, .5, .5, .5)
+
+    # draw the latest bullet
+    fill_circle(math.sin(phi_rad) * r, math.cos(phi_rad) * r + 60, 20, .9, .0, .0)
+
+    # saving bullet pos for further rounds
+    start_pos += i
+    start_pos %= 7
+    return start_pos
+
+
+draw_baraban()
+
+last_bullet_pos = 0
 
 answer = ''
 while answer != 'n':
-    answer = tkSimpleDialog.askstring('Draw the circle?', 'y/n')
+    answer = tkSimpleDialog.askstring('Lets play a game?', 'y/n')
     if answer == 'y':
-        turtle.penup()
-        turtle.goto(random.randrange(-300, 300), random.randrange(-200, 200))
-        turtle.pendown()
-        turtle.fillcolor(random.random(), random.random(), random.random())
-        turtle.begin_fill()
-        for i in range(1, 2):
-            turtle.circle(random.randrange(1, 100))
-        turtle.end_fill()
+
+        # animate the rotating baraban
+        last_bullet_pos = animate_baraban(last_bullet_pos)
+        # check if u ded, lik so ded
+        if last_bullet_pos % 7 == 0:
+            turtle.penup()
+            turtle.goto(-50, 200)
+            turtle.pendown()
+            turtle.write("U DED (x_x)", font=("Arial", 18, "normal"))
+
     else:
         break
