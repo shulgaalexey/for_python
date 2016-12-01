1e7d5cb345dae55ccdf63222bdd766df830526de
shulgaalexey <shulgaalexey@gmail.com>
2016-12-01 19:29:48 +0900

    Extracted functions for drawing circle and filled circle

--- les06.py
+++ les06.py
@@ -8,6 +8,21 @@ root.withdraw()
 
 turtle.speed(0)
 
+def draw_circle(posx, posy, r):
+    turtle.penup()
+    turtle.goto(posx, posy)
+    turtle.pendown()
+    turtle.circle(r)
+
+def fill_circle(posx, posy, radius, r, g, b):
+    turtle.fillcolor(r, g, b)
+    turtle.begin_fill()
+    draw_circle(posx, posy, radius)
+    turtle.end_fill()
+
+fill_circle(0, 0, 80, 1, 0, 0)
+
+
 answer = ''
 while answer != 'n':
     answer = tkSimpleDialog.askstring('Draw the circle?', 'y/n')
@@ -15,7 +30,10 @@ while answer != 'n':
         turtle.penup()
         turtle.goto(random.randrange(-300, 300), random.randrange(-200, 200))
         turtle.pendown()
-        for i in range(1, 3):
+        turtle.fillcolor(random.random(), random.random(), random.random())
+        turtle.begin_fill()
+        for i in range(1, 2):
             turtle.circle(random.randrange(1, 100))
+        turtle.end_fill()
     else:
         break
